1616f3210bf1dc8a536243570c63f83d96658423
Diogolima-creator <dbznetudo@gmail.com>
2024-04-20 19:20:25 -0400

    results

--- imprimirResult.py
+++ imprimirResult.py
@@ -1,17 +1,18 @@
+import os
+
 def imprimirResult(nome_arquivo, fc, solucao):
+    nome_arquivo = os.path.join('results', nome_arquivo + '.txt')  # Concatenando o nome do arquivo com o diretório
+    # Convertendo os valores para strings
+    fc2_str = str(fc)
+    solucao_str = ' '.join(map(str, solucao))  # Convertendo a lista em uma única string
 
-  nome_arquivo += '.txt'
-  # Convertendo os valores para strings
-  fc2_str = str(fc)
-  solucao_str = ' '.join(map(str, solucao))  # Convertendo a lista em uma única string
+    # Criando a lista de strings para escrever no arquivo
+    arrayTxt = [fc2_str, solucao_str]
 
-  # Criando a lista de strings para escrever no arquivo
-  arrayTxt = [fc2_str, solucao_str]
+    # Abre o arquivo em modo de escrita ('w' significa write)
+    with open(nome_arquivo, 'w') as arquivo:
+        # Escreve os dados no arquivo
+        arquivo.write('\n'.join(arrayTxt))
 
-  # Abre o arquivo em modo de escrita ('w' significa write)
-  with open(nome_arquivo, 'w') as arquivo:
-      # Escreve os dados no arquivo
-      arquivo.write('\n'.join(arrayTxt))
-
-  print("Solução:", solucao)
-  print("Custo da solução:", fc)
+    print("Solução:", solucao)
+    print("Custo da solução:", fc)
